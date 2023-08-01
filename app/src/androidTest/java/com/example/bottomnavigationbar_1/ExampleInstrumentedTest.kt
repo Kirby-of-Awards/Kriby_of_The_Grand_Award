a82badc2a8b95770585ff1c6530bd8a566b9424c
hm <panmahm@naver.com>
2023-08-01 23:53:47 +0900

    final

--- app/src/androidTest/java/com/example/bottomnavigationbar_1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/bottomnavigationbar_1/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.bottomnavigationbar_1
+package com.example.main_bottom
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.bottomnavigationbar_1", appContext.packageName)
+        assertEquals("com.example.main_bottom", appContext.packageName)
     }
 }
